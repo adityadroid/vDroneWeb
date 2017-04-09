dec5ff6b062381a0ce84c130f7b57925000131e3
eminemoholic <adityaadi1467@gmail.com>
2017-04-09 13:44:46 +0530

    Added login

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 var app = angular.module('vDrone',['ngMaterial','ngAnimate','ngAria']);
 app.controller('loginController',function( $scope ) {
     $scope.user = {
-      name : "Naomi Black",
+      name : "",
       password: ""
     }
   });
